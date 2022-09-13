0c5fc6168519d0baa81316ceda239bf3751b017e
karl wernsman <wernsmank@gmail.com>
2022-09-13 15:34:35 -0700

    added HTML

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 /* Imports */
 
 /* State */
-
+let gameState = ''; //choose or results
 /* Actions */
 function loadPage() {}
 
 /* Components */
+const stoneImg = document.getElementById('stone-img');
+const clothImg = document.getElementById('cloth-img');
+const scytheImg = document.getElementById('scythe-img');
 
 /* Component */
 // get DOM
 // display
-// event listeners
+function displayChoice() {
+    stoneImg.classList.remove('hide');
+    clothImg.classList.remove('hide');
+    scytheImg.classList.remove('hide');
+}
 
+// event listeners
+stoneImg.addEventListener('click', () => {
+    displayChoice();
+});
 /* Run page load code */
 loadPage();
