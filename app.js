d28683ebe4e7aa436820969cfc8d859c063cab99
karl wernsman <wernsmank@gmail.com>
2022-09-13 16:16:38 -0700

    Updated HTML and added eventlisteners

--- app.js
+++ app.js
@@ -1,27 +1,61 @@
 /* Imports */
 
 /* State */
-let gameState = ''; //choose or results
-/* Actions */
-function loadPage() {}
+let gameState = 'choice'; //choose or results
+let choice = ''; //stone, cloth, or scythe
+let computerChoice = 'stone'; //
 
+/* Actions */
+function loadPage() {
+    displayResult();
+}
+
+function playAgain() {
+    gameState;
+    loadPage();
+}
 /* Components */
 const stoneImg = document.getElementById('stone-img');
 const clothImg = document.getElementById('cloth-img');
 const scytheImg = document.getElementById('scythe-img');
 
+const stone = document.getElementById('choose-stone');
+const cloth = document.getElementById('choose-cloth');
+const scythe = document.getElementById('choose-scythe');
+
+const display1 = document.getElementById('display-1');
+
+const playAgainButton = document.getElementById('play-again');
+
 /* Component */
 // get DOM
 // display
-function displayChoice() {
+function displayResult() {
     stoneImg.classList.remove('hide');
     clothImg.classList.remove('hide');
     scytheImg.classList.remove('hide');
 }
 
+if (choice === computerChoice) {
+    display1;
+}
+
 // event listeners
-stoneImg.addEventListener('click', () => {
-    displayChoice();
+playAgainButton.addEventListener('click', () => {
+    playAgain();
+});
+
+stone.addEventListener('click', () => {
+    choice = 'stone';
+    gameState = 'results';
+});
+cloth.addEventListener('click', () => {
+    choice = 'cloth';
+    gameState = 'results';
+});
+scythe.addEventListener('click', () => {
+    choice = 'scythe';
+    gameState = 'results';
 });
 /* Run page load code */
 loadPage();
